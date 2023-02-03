11aa41a28444d15d72aed6eab439ba7121db404b
r3is3rsf <robertoperez297@gmail.com>
2023-02-02 23:25:22 -0400

    second commit, center content

--- src/App.tsx
+++ src/App.tsx
@@ -18,21 +18,34 @@ function App() {
       
       <div className='row  vh-100 justify-content-center align-items-center'>
         <div className="card col-auto h-75" >
-           <div className="card-body justify-content-center align-items-center">
-              <div className='d-flex text-secondary col-auto'>
-                  <img className="card-img-top w-25 border border-2 rounded-circle" src={img} alt="Card image cap"/>
-                  <div>
+           <div className='row vh-100 row justify-content-center  align-items-center'>
+              <div className="card-body col-auto justify-content-center align-items-center">
+              <div className='d-flex justify-content-center text-secondary'>
+
+                  <img className="d-flex card-img-top w-25 border border-2 rounded-circle me-4" src={img} alt="Card image cap"/>
+                  <div className=''>
                       <h1 className="card-title">- Jeidy Art -</h1>
-                      <h6 className="card-text">DIGITAL ARTIST</h6>
-                      <h6 className="card-text">SHE/HER ┊ ENG/SPA</h6>
                       <p className="card-text">DIGITAL ARTIST</p>
+                      <p className="card-text">SHE/HER ┊ ENG/SPA</p>
+                      <hr/>
+                      <span>
+                      I'm Jeidy, a freelance artist.
+                        <br />
+                      You can consult my prices and check my art.
+                        <br />
+                      Commission Status: CHECK FIRST
+                        <br />
+                      Waitlist: OPEN
+                      </span>
 
                   </div>
               </div>
-              <div className=''>
+              <hr/>
+              <div className='d-flex justify-content-center'>
                   <a href="#" className="btn btn-outline-dark me-2">T.O.S</a>
                   <a href="#" className="btn btn-outline-dark me-2">Contact</a>
                   <a href="#" className="btn btn-outline-dark me-2">Prices</a>
+              </div>
               </div>
 
            </div>
